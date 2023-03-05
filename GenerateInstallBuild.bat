d80dd0921b7eaa86ff43b201fba137e3a08c24b3
DaiMingze <anyingshashou@qq.com>
2023-03-05 16:45:55 +0800

    update installbuild
    update installbuild

--- GenerateInstallBuild.bat
+++ GenerateInstallBuild.bat
@@ -3,7 +3,7 @@ REM Install Build UE5.1.1
 REM fix .NetFramework 4.5 to 4.8
 
 cd /d Engine/Binaries/DotNET/AutomationTool
-AutomationTool.exe BuildGraph -target="Make Installed Build Win64" -script=Engine/Build/InstalledEngineBuild.xml -set:WithWin64=true -set:WithMac=false -set:WithAndroid=false -set:WithIOS=false -set:WithTVOS=false -set:WithLinux=true -set:WithLinuxArm64=false -set:WithServer=true -clean
+AutomationTool.exe BuildGraph -target="Make Installed Build Win64" -script=Engine/Build/InstalledEngineBuild.xml -set:WithWin64=true -set:WithMac=false -set:WithAndroid=false -set:WithIOS=false -set:WithTVOS=false -set:WithLinux=true -set:WithLinuxArm64=false -set:WithServer=true -set:WithFullDebugInfo=false -set:GameConfigurations=Shipping;Development;DebugGame -clean
 echo.  
 echo. Install Build Generate Script Complete
 pause 
