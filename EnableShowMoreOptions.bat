7257c5943f754a325c84f7b044dbce38ed3df47f
DaiMingze <anyingshashou@qq.com>
2023-03-06 23:26:01 +0800

    Update EnableShowMoreOptions.bat

--- EnableShowMoreOptions.bat
+++ EnableShowMoreOptions.bat
@@ -1,1 +1,5 @@
+REM Open Windows Terminal, and select Windows PowerShell or Command Prompt.
+REM Copy and paste the command below you want into Windows Terminal, and press Enter
 reg delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
+REM Close Windows Terminal.
+REM You may need to either restart the explorer process, sign out and sign in, or restart the computer to apply.
